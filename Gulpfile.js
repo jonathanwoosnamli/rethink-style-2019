d8a4b98a32eb8c1244c0d6053a4efa70884963af
Jonathan Woosnam-Li <Jonathan.Woosnam-Li@rethink.org>
2019-02-27 16:05:24 +0000

    CSS started for out of scope sites

---
+++ Gulpfile.js
@@ -0,0 +1,41 @@
+var gulp         = require('gulp'),
+	postcss 	 = require('gulp-postcss'),
+	uglify       = require('gulp-uglify'),
+	plumber      = require('gulp-plumber'),
+	rename       = require('gulp-rename'),
+	autoprefixer = require('autoprefixer'),
+	notify       = require('gulp-notify'),
+	sass         = require('gulp-sass'),
+	concat       = require('gulp-concat'),
+	lost         = require('lost');
+
+var watch_paths = {
+	//scripts: ['assets_src/js/modules/*.js'],
+	styles:  ['*.scss']
+};
+
+
+// Styles Task
+gulp.task('styles', function() {
+	var processors = [
+		lost,
+		autoprefixer({browsers:['last 6 version', 'ie >= 9']})
+	];
+
+	gulp.src(watch_paths.styles)
+		.pipe(plumber())
+		.pipe(sass({
+			outputStyle: 'compressed' // Uncomment to minify css
+		}))
+		.pipe(postcss(processors))
+		.pipe(gulp.dest('css'))
+		.pipe(notify({ message: 'Styles task complete' }));
+});
+
+// Watch Task
+gulp.task('watch', function() {
+	//gulp.watch(watch_paths.scripts, ['scripts']);
+	gulp.watch(watch_paths.styles, ['styles']);
+});
+
+gulp.task('default', ['watch']);
